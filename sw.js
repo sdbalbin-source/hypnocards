d75006d97e6ac38801009b21aef9fb590139306f
sdbalbin-source <sdbalbin@gmail.com>
2026-03-08 21:48:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,8 @@
-const CACHE = 'hypnocards-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
-
+const CACHE = 'hypnocards-v2';
+const ASSETS = ['/', '/index.html', '/cards_data.json', '/manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
